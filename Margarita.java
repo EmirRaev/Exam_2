e747d53e04927fc9c481a8b223317d36044529a1
Emir <104508902+EmirRaev@users.noreply.github.com>
2022-05-31 21:52:17 +0600

    EmirKazybekov

---
+++ Margarita.java
@@ -0,0 +1,19 @@
+public class Margarita extends Pizza{
+    @Override
+    void Label() {
+        System.out.println("Pizza: Margarita"+"\nweight: "+getWeight()+"\nprice:"+getPrice());
+    }
+    int  Special(int num) {
+        if (num == 1) {
+            System.out.println("С мясом");
+            return num;
+        } else if (num == 2) {
+            System.out.println("Без мяса");
+            return num;
+        }
+        else {
+            System.out.println("По умолчанию идет без мяса");
+        }
+        return num;
+    }
+}
